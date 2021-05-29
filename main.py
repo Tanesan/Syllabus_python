4695d31aa5ebe4d2150a6ea29459a4fc1afd4afe
Keito <54165015+Tanesan@users.noreply.github.com>
2021-05-29 15:15:22 +0900

    W

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     # options.add_argument('disable-gpu')
-    # options.add_argument('--no-sandbox')
+    options.add_argument('--no-sandbox')
     # options.add_argument("start-maximized")
     # options.add_argument("--single-process")
     # options.add_argument("enable-automation")
