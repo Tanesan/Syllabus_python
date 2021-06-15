be04b747fc8224e1118971198046ce9f261d8984
Keito <54165015+Tanesan@users.noreply.github.com>
2021-06-15 23:04:24 +0900

    data uploaded (4)

--- main0.py
+++ main0.py
@@ -1,7 +1,7 @@
 from define import act
 import os
-a = [97]
+a = [21]
 if os.path.isfile("docs/" + str(a[0]) + '.json'):
     os.remove("docs/" + str(a[0]) + '.json')
 for i in a:
-    act(i, 3, 4)
+    act(i, 0, 550)
---
+++ main59.py
@@ -0,0 +1,7 @@
+from define import act
+import os
+a = [36]
+if os.path.isfile("docs/" + str(a[0]) + '.json'):
+    os.remove("docs/" + str(a[0]) + '.json')
+for i in a:
+    act(i, 0, 550)
