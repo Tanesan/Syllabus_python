f51df752d734136742ed7b5f30b2396b1339a94f
Keito <54165015+Tanesan@users.noreply.github.com>
2021-08-06 17:22:17 +0900

    W

--- define.py
+++ define.py
@@ -13,8 +13,8 @@ def act(m, a, b):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(options=options)
-    # driver = webdriver.Chrome(executable_path='/Users/keitotanemura/Downloads/chromedriver', options=options)
+    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(executable_path='/Users/keitotanemura/Downloads/chromedriver2', options=options)
     # print("A")
 
     # for m in [21, 22, 23, 24, 25, 26, 28, 29, 31, 32, 34, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
@@ -145,12 +145,12 @@ def act(m, a, b):
         sleep(1)
         data_all = {}
         data_all.update(data)
-        if os.path.isfile("docs/all.json") and os.stat("docs/all.json").st_size > 0:
-            json_open_all = open("docs/all.json", 'r')
-            json_load_all = json.load(json_open_all)
-            data_all.update(json_load_all)
-            with open("docs/all.json", 'w') as f:
-                json.dump(data_all, f, ensure_ascii=False)
+        print(data_all)
+        json_open_all = open("docs/all.json", 'r')
+        json_load_all = json.load(json_open_all)
+        json_load_all.update(data_all)
+        with open("docs/all.json", 'w') as f:
+            json.dump(json_load_all, f, ensure_ascii=False)
     # if os.path.isfile("docs/" + str(m) + '.json') and os.stat("docs/" + str(m) + '.json').st_size > 0:
     #     json_open = open("docs/" + str(m) + '.json', 'r')
     #     json_load = json.load(json_open)
