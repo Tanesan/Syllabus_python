fe11a75e72e1877eacbd9fdfe6a8b89675dc7b81
Keito <54165015+Tanesan@users.noreply.github.com>
2021-08-17 22:24:18 +0900

    renewed

--- sitemap.py
+++ sitemap.py
@@ -12,7 +12,7 @@ df = pandas.read_json(json_open_all)
 urls = []
 
 for i in df.columns.values.tolist():
-    urls.append("https://univ-syllabus.web.app/subject/" + i)
+    urls.append("https://kgu-syllabus.com/subject/" + i)
 
 urlset = ET.Element('urlset')
 urlset.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
@@ -20,9 +20,9 @@ tree = ET.ElementTree(element=urlset)
 
 url_element = ET.SubElement(urlset, 'url')
 loc = ET.SubElement(url_element, 'loc')
-loc.text = "https://univ-syllabus.web.app/"
+loc.text = "https://kgu-syllabus.com/"
 lastmod = ET.SubElement(url_element, 'lastmod')
-lastmod.text = nt[:nt.find('.')]
+lastmod.text = "2021-08-17"
 priority = ET.SubElement(url_element, 'priority')
 priority.text = "1.00"
 
@@ -31,7 +31,7 @@ for url in urls:
     loc = ET.SubElement(url_element, 'loc')
     loc.text = url
     lastmod = ET.SubElement(url_element, 'lastmod')
-    lastmod.text = nt[:nt.find('.')]
+    lastmod.text = "2021-08-17"
     priority = ET.SubElement(url_element, 'priority')
     priority.text = "0.80"
 
