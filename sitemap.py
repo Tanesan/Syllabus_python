5fed857ce9fef391feb934b9d912b4417a36be59
Keito <54165015+Tanesan@users.noreply.github.com>
2021-08-17 21:44:07 +0900

    renewed

--- sitemap.py
+++ sitemap.py
@@ -1,14 +1,14 @@
 import xml.etree.ElementTree as ET
 
 import pandas
+import datetime
 
+dt_now = datetime.datetime.now()
 json_open_all = open("docs/all.json", 'r')
 
 df = pandas.read_json(json_open_all)
 
-urls = [
-    "https://univ-syllabus.web.app/",
-]
+urls = []
 
 for i in df.columns.values.tolist():
     urls.append("https://univ-syllabus.web.app/subject/" + i)
@@ -17,13 +17,21 @@ urlset = ET.Element('urlset')
 urlset.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
 tree = ET.ElementTree(element=urlset)
 
+url_element = ET.SubElement(urlset, 'url')
+loc = ET.SubElement(url_element, 'loc')
+loc.text = "https://univ-syllabus.web.app/"
+lastmod = ET.SubElement(url_element, 'lastmod')
+lastmod.text = "2021-08-17T09:34:48+00:00"
+priority = ET.SubElement(url_element, 'priority')
+lastmod.text = "1.00"
+
 for url in urls:
     url_element = ET.SubElement(urlset, 'url')
     loc = ET.SubElement(url_element, 'loc')
     loc.text = url
     lastmod = ET.SubElement(url_element, 'lastmod')
-    lastmod.text = "2021-08-17T09:34:48+00:00"
+    lastmod.text = dt_now.isoformat()
     priority = ET.SubElement(url_element, 'priority')
-    lastmod.text = "1.00"
+    lastmod.text = "0.80"
 
 tree.write('sitemap.xml', encoding='utf-8', xml_declaration=True)
