fda8bc4f41af60636ff11fb69af0aef81f156c14
Keito <54165015+Tanesan@users.noreply.github.com>
2021-09-06 15:58:19 +0900

    delete files

--- define.py
+++ define.py
@@ -171,6 +171,9 @@ def act(m, a, b):
         #         }
         #     })
         json_load_all = json.load(json_open_all)
+        for n in range(len(json_load_all)):
+            if json_load_all[n]["管理部署"] == data_all[0]["管理部署"]:
+                del json_load_all[n]
         json_load_all.update(data_all)
         with open("docs/all.json", 'w') as f:
             json.dump(json_load_all, f, ensure_ascii=False)
