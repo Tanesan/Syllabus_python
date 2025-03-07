03d56e135721b5ee1ee1246e30963955f7344256
Keito Tanemura <54165015+Tanesan@users.noreply.github.com>
2025-03-07 09:05:56 +0900

    change 2025

--- define.py
+++ define.py
@@ -216,7 +216,7 @@ def act(m, a, b):
         # 年度設定
         year_2022 = driver.find_element_by_id("txtLsnOpcFcy")
         year_2022.clear()
-        year_2022.send_keys("2024")
+        year_2022.send_keys("2025")
 
         driver.find_element_by_name('ESearch').click()
         for a in range(int(i / 100)):
